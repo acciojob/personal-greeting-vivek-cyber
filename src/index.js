bd89127af529f66b56af40a2a8fe5e93e7bf57ad
surya <suryavivek85@gmail.com>
2023-07-12 23:59:46 +0530

    submitting code-Wed Jul 12 2023 23:59:45 GMT+0530 (India Standard Time)

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ document.getElementById("input").addEventListener("keyup",dis)
 function dis(event)
 {
   let d=document.getElementById("display")
-  d.innerText=event.target.value
+  d.innerText="Hello "+event.target.value+"!"
 }
